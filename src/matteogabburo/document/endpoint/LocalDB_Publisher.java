68a30bbcf68c04774ab717af221d59aa7adcf140
Matteo <matteo@localhost.localdomain>
2016-08-10 14:36:14 +0200

    Publisher done

--- src/matteogabburo/document/endpoint/LocalDB_Publisher.java
+++ src/matteogabburo/document/endpoint/LocalDB_Publisher.java
@@ -1,5 +1,33 @@
 package matteogabburo.document.endpoint;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+
+import javax.xml.ws.Endpoint;
+
+import matteogabburo.document.ws.LocalDB_implementation;
+
 public class LocalDB_Publisher {
 
+	public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        if (HOSTNAME.equals("127.0.0.1"))
+        {
+            HOSTNAME = "localhost";
+        }
+        String PORT = "6902";
+        String BASE_URL = "/ws/local-db-service";
+
+        if (String.valueOf(System.getenv("PORT")) != "null"){
+            PORT=String.valueOf(System.getenv("PORT"));
+        }
+        
+        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
+        System.out.println("Starting local-db-service...");
+        System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
+        Endpoint.publish(endpointUrl, new LocalDB_implementation());
+    }
+	
 }
