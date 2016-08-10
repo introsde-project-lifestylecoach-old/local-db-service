229b61d1432c3742aa0a53c3467b6c1ca2ce2a94
Matteo <matteo@localhost.localdomain>
2016-08-10 14:33:49 +0200

    update ws interface and implementation

--- src/matteogabburo/document/ws/LocalDB_implementation.java
+++ src/matteogabburo/document/ws/LocalDB_implementation.java
@@ -1,5 +1,34 @@
 package matteogabburo.document.ws;
 
-public class LocalDB_implementation {
+import javax.jws.WebService;
+
+import matteogabburo.document.models.User;
+
+@WebService(endpointInterface = "matteogabburo.document.ws.LocalDB_interface", serviceName="local-db-service")
+public class LocalDB_implementation implements LocalDB_interface{
+
+	@Override
+	public User get_user_by_id(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User get_user_by_auth(String mail, String passwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User update_user(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User delete_user(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/matteogabburo/document/ws/LocalDB_interface.java
+++ src/matteogabburo/document/ws/LocalDB_interface.java
@@ -1,5 +1,31 @@
 package matteogabburo.document.ws;
 
-public class LocalDB_interface {
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
+import matteogabburo.document.models.User;
+
+@WebService
+@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL) //optional
+public interface LocalDB_interface {
+	
+	@WebMethod(operationName="get_user_by_id")
+    @WebResult(name = "User")
+	User get_user_by_id(Long id);
+	
+	@WebMethod(operationName="get_user_by_auth")
+    @WebResult(name = "User")
+	User get_user_by_auth(String mail, String passwd);
+	
+	@WebMethod(operationName="update_user")
+    @WebResult(name = "User")
+	User update_user(User user);	
+	
+	@WebMethod(operationName="delete_user")
+    @WebResult(name = "User")
+	User delete_user(User user);	
 }
